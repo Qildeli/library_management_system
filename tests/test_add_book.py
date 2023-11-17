5ab7b9c1986050ac354889ed370291faa7741a17
Qildeli <anaqildeli@gmail.com>
2023-11-17 17:43:18 +0400

    Initial commit

--- tests/test_add_book.py
+++ tests/test_add_book.py
@@ -1,6 +1,11 @@
 import unittest
 from library.library import Library
 
+"""
+Test adding a book with valid details and ensure it is saved to the books dictionary and the JSON file.
+Test adding a book with an ID that already exists and ensure it handles duplicates appropriately.
+"""
+
 
 class TestAddBook(unittest.TestCase):
 
